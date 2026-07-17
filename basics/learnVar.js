1ace5cb48c19f5e04f6ce19181850423358bd4d4
parvesh <Parvesh.sangwan@gmail.com>
2026-07-17 17:22:32 +0530

    variable

---
+++ basics/learnVar.js
@@ -0,0 +1,58 @@
+
+let num=2;
+// console.log(num);
+num=4;
+
+// let num1=num;
+// let userName='parvesh\'s sangwan';
+
+let passwordflex122="login123";
+
+// can't use the .dot
+// digit is not allowed in the begning 
+//$ or _ in the variable name
+
+// let UserName="nike";
+
+let user_name='mike';//snake caseing 
+let userPassword='login'//camel caseing
+// let userName="nitika";
+/*
+console.log(userName);
+console.log(UserName);
+ console.log(num);
+
+// console.log(num+2);
+*/
+
+// we define the variable in two ways
+// let and var
+
+//var --- scope
+//use strict
+
+let UserName="nike";
+
+var adminUser="sam";
+console.log(adminUser);
+
+var adminUser="jim";
+console.log(adminUser);
+
+if(5>3){
+    var greaterNum=5;
+    // console.log(greaterNum);
+}
+
+ console.log(greaterNum);
+ 
+// learn constant
+ let radius;
+ const pi="text";
+radius=2;
+pi=3;
+//  pi=6.12;
+
+ let area= pi*radius*radius;
+
+ console.log(area);
