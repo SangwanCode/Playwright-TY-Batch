35bb970b8bc01ea1653df4ceb0d9c1011d625595
parvesh <Parvesh.sangwan@gmail.com>
2026-07-26 10:49:06 +0530

    more operator and comaparisonOperator

---
+++ basics/Comparsion.js
@@ -0,0 +1,33 @@
+// >, <,>=, <=,==,===
+
+// let a=4;
+// let b=6;
+// console.log(a>b);//false
+// console.log(a<b);//true
+// console.log(a<=b);
+
+let num="1";
+//js convert the string into number and then compare it
+// console.log(typeof num)
+// let num1=Number(num)
+// console.log(typeof num1)
+// console.log(num==1);//false, true
+
+let a=0;
+// console.log(Boolean(a)); // false
+// console.log(typeof a)
+let b="0";
+// console.log(Boolean(b))// true
+
+//  console.log(a==b); //true
+
+ //===
+// console.log(a===b);
+
+let name="p"
+let lastname=12
+
+console.log(name==lastname);
+
+
+
---
+++ basics/moreOperations.js
@@ -0,0 +1,60 @@
+// Assignment operator
+
+// =
+// operator return value
+
+let num=2;
+// console.log(num);
+
+let result;
+2+3;
+// console.log(result);
+
+// chaning Operator
+
+let a,b,c;
+a=b=c=3;// not used or recommended 
+// console.log(`value of ${a} then value of b is ${b} and c is ${c}`);
+// console.log(b);
+// console.log(c);
+
+// modify in place
+
+let n=2;
+// let m=n+5;// 2+5=7
+//  n=n+5;
+//  n+=5; //n=n+5;
+//  n=n*2;  // same as n*=2
+// n*=2
+n*=3+5 //n*=8 //n=n*8
+// n? is that sring ?
+// n=m;
+// console.log(n);
+
+// increment and decrement "Post(M++)"and "pre(++m)"
+
+let m=3;
+// m=m+1;
+// m+=1;
+ // m++; //++ means increase the number by 1
+// ++m  //pre increament
+// console.log(m)
+// console.log(++m)
+
+
+// let num1=m++; //post
+let num1=m++; //m+=1
+// console.log(num1);
+//  console.log(`num1 value is ${num1} and m val is ${m}`);
+// num1=3, m=4
+
+// js post increment  use value and increment the value.
+
+
+let G=5;
+/*
+console.log(G++);//5,6,6
+num3=G++;
+console.log(G);//6,5
+*/
+console.log(++G);
