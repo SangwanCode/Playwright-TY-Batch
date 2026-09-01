2d17a42c73a7abb19e71e72b20fdb501b70e62fc
parvesh <Parvesh.sangwan@gmail.com>
2026-09-01 10:19:49 +0530

    deepcloning

--- object/referenceObj.js
+++ object/referenceObj.js
@@ -21,11 +21,11 @@
 // let a={age:30};
 // let b=a;
 
-// console.log(a==b)
+// console.log(a==b) // true
 
 // let a={}
 // let b={}
-// console.log(a===b)
+// console.log(a===b) //false
 
 // cloning --duplicate and merging
 
@@ -86,11 +86,63 @@
 
 // 3. spread operator ...
 
-let user={
-    name:'john'
-}
-let clone={...user}
+// let user={
+//     name:'john',  
+// }
+
+// let clone={...user}
 // console.log(clone)
-clone.name='peter'
-console.log(clone)
-console.log(user)
+// clone.name='peter'
+// console.log(clone)
+// console.log(user)
+
+// multiple objects
+// let user={
+//     name:'john',  
+// }
+// let permission1={'canview':true};
+// let permission2={'canEdit':true};
+// let clone={...user,...permission1,...permission2}
+//  console.log(clone)
+//  console.log(permission1==clone)
+
+// Nested clone
+
+// let user={ 
+//     name:'john',
+//     age:30,
+//     address:{
+//         city:'CHD',
+//         pincode:12345
+//     }
+// }
+// // console.log(user.address.city)
+// let clone=Object.assign({},user);
+// // console.log(clone)
+//  console.log(clone.address==user.address) // true
+// clone.name='peter'
+// clone.address.city='Mohali'
+// console.log(clone==user)// false both are independent
+//  console.log(user.name)
+//  console.log(user.address.city)
+
+// Shallow cloning
+
+//deep cloning : StructuredClone
+
+let user={ 
+    name:'john',
+    age:30,
+    address:{
+        city:'CHD',
+        pincode:12345
+    }
+}
+let clone=structuredClone(user);
+// console.log(clone.address==user.address) //false both are indepndent
+clone.address.city="mohali";
+console.log(clone.address.city)
+console.log(user.address.city)
+
+
+
