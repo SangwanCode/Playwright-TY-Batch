c06644761c7c0f1447e7e31cc86cc90f5944f12e
parvesh <Parvesh.sangwan@gmail.com>
2026-09-15 10:37:04 +0530

    array

---
+++ array/basic.js
@@ -0,0 +1,65 @@
+// object--collection of pairs {}
+// ordered collection.
+// Array : collections of values []
+
+// Declaration
+
+// let arr=[]
+// let arr1=new Array();
+
+// insert values
+// let brw=['ch','ff','edge']
+// console.log(brw)
+// console.log(brw[0])
+// let arr=[]
+// arr[0]='apple'
+// arr[1]='mango'
+
+// console.log(arr)
+
+// replace 
+// let brw=['ch','ff','edge']
+// console.log(brw)
+// brw[1]='safari'
+// console.log(brw)
+
+// brw[5]="opera"// will add on 5 position and leave 3 and 4 empty
+// console.log(brw)
+
+// to check the length : length 
+
+// let brw=['ch','ff','edge']
+// console.log(brw.length)
+
+// can store anything
+
+// let arr=["parvesh",34,true,function sayHI(){console.log('Hi')},'chrome']
+
+// console.log(arr)
+// arr[3]();
+// console.log(arr[2])
+
+// function greet(){
+//     console.log("hi")
+// }
+// let fn=greet;
+// fn();
+
+//  at: used for get the last (-1)
+
+// let brw=['ch','ff','edge']
+
+// console.log(brw.at(-1))
+// console.log(brw[brw.length-1])
+
+// console.log(brw.at(1))
+
+
+
+
+
+
+
+
+
+
---
+++ array/methods.js
@@ -0,0 +1,25 @@
+// methods: Push And Pop
+
+// Push -- append in last of the array
+
+// let arr=[1,3,5,7]
+// console.log(arr)
+// arr.push(9)
+// // console.log(arr.push(9))
+// console.log(arr)
+
+let brw=[]
+
+// brw.push('ch'); 
+// brw.push('ff');
+// brw.push('edge');
+brw.push('ch',"ff",'edge'); 
+ console.log(brw)
+
+// pop --remove last value of array
+
+brw.pop(); // edge
+console.log(brw.pop()) //ff
+console.log(brw)
+//2 ch ff
+
